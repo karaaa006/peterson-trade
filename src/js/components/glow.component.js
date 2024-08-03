6ac421b743e2cca7f3922eaf107d846a963948a8
Oleh Karaulnyi <olegkaraaa@gmail.com>
2024-08-03 20:14:10 +0300

    university pade desctop: hero, instruments, presentation, tariffs, reviews (started)

--- src/js/components/glow.component.js
+++ src/js/components/glow.component.js
@@ -2,77 +2,79 @@ import gsap from 'gsap';
 
 const scalpingSection = document.querySelector('#scalping');
 
-let svgns = 'http://www.w3.org/2000/svg';
-let root = document.querySelector('.glow-svg');
-let ease = 0.75;
+if (scalpingSection) {
+  let svgns = 'http://www.w3.org/2000/svg';
+  let root = document.querySelector('.glow-svg');
+  let ease = 0.75;
 
-let pointer = {
-  x: window.innerWidth / 2,
-  y: window.innerHeight / 2,
-};
+  let pointer = {
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2,
+  };
 
-const handleMouseMove = (event) => {
-  const scalpingSectionRect = scalpingSection.getBoundingClientRect();
+  const handleMouseMove = (event) => {
+    const scalpingSectionRect = scalpingSection.getBoundingClientRect();
 
-  if (event.clientY - scalpingSectionRect.top <= 300) {
-    gsap.to('line', { opacity: 0, y: 300 });
-  } else if (
-    scalpingSectionRect.top + scalpingSectionRect.height - event.clientY <=
-    300
-  ) {
-    gsap.to('line', {
-      opacity: 0,
-      y: scalpingSectionRect.height - 300,
-    });
-  } else {
-    gsap.to('line', { opacity: 1 });
+    if (event.clientY - scalpingSectionRect.top <= 300) {
+      gsap.to('line', { opacity: 0, y: 300 });
+    } else if (
+      scalpingSectionRect.top + scalpingSectionRect.height - event.clientY <=
+      300
+    ) {
+      gsap.to('line', {
+        opacity: 0,
+        y: scalpingSectionRect.height - 300,
+      });
+    } else {
+      gsap.to('line', { opacity: 1 });
+    }
+    pointer.x = event.clientX - scalpingSectionRect.left;
+    pointer.y = event.clientY - scalpingSectionRect.top;
+  };
+
+  scalpingSection.addEventListener('mousemove', handleMouseMove);
+
+  let leader = (prop) => {
+    return prop === 'x' ? pointer.x : pointer.y;
+  };
+
+  let total = 150;
+  for (let i = 0; i < total; i++) {
+    leader = createLine(leader, i);
   }
-  pointer.x = event.clientX - scalpingSectionRect.left;
-  pointer.y = event.clientY - scalpingSectionRect.top;
-};
 
-scalpingSection.addEventListener('mousemove', handleMouseMove);
+  const createLine = (leader, i) => {
+    let line = document.createElementNS(svgns, 'line');
+    root.appendChild(line);
 
-let leader = (prop) => {
-  return prop === 'x' ? pointer.x : pointer.y;
-};
+    gsap.set(line, { x: -15, y: -15, opacity: (total - i) / total });
 
-let total = 150;
-for (let i = 0; i < total; i++) {
-  leader = createLine(leader, i);
-}
+    let pos = gsap.getProperty(line);
 
-function createLine(leader, i) {
-  let line = document.createElementNS(svgns, 'line');
-  root.appendChild(line);
-
-  gsap.set(line, { x: -15, y: -15, opacity: (total - i) / total });
-
-  let pos = gsap.getProperty(line);
-
-  gsap.to(line, {
-    duration: 0.1,
-    x: '+=1',
-    y: '+=1',
-    repeat: -1,
-    ease: 'none',
-    modifiers: {
-      x: () => {
-        let posX = pos('x');
-        let leaderX = leader('x');
-        let x = posX + (leaderX - posX) * ease;
-        line.setAttribute('x2', leaderX - x);
-        return x;
+    gsap.to(line, {
+      duration: 0.1,
+      x: '+=1',
+      y: '+=1',
+      repeat: -1,
+      ease: 'none',
+      modifiers: {
+        x: () => {
+          let posX = pos('x');
+          let leaderX = leader('x');
+          let x = posX + (leaderX - posX) * ease;
+          line.setAttribute('x2', leaderX - x);
+          return x;
+        },
+        y: () => {
+          let posY = pos('y');
+          let leaderY = leader('y');
+          let y = posY + (leaderY - posY) * ease;
+          line.setAttribute('y2', leaderY - y);
+          return y;
+        },
       },
-      y: () => {
-        let posY = pos('y');
-        let leaderY = leader('y');
-        let y = posY + (leaderY - posY) * ease;
-        line.setAttribute('y2', leaderY - y);
-        return y;
-      },
-    },
-  });
+    });
 
-  return pos;
+    return pos;
+  };
 }
